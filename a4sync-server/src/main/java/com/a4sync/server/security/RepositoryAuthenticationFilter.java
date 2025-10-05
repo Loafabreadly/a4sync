893bf739d5f6b932f4d201776b6ff00b9e7f98dc
Loafabreadly <birdgeekthree@gmail.com>
2025-10-05 09:20:29 -0600

    fix: Update JDK version in release workflow and remove unused BCrypt import in RepositoryAuthenticationFilter

--- a4sync-server/src/main/java/com/a4sync/server/security/RepositoryAuthenticationFilter.java
+++ a4sync-server/src/main/java/com/a4sync/server/security/RepositoryAuthenticationFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.lang.NonNull;
 
